48ad104175f1cb9a143b478ba502adfa7883d3f3
pedro.dias <pedro.dias@newvision.pt>
2021-08-01 19:43:43 +0100

    created device data

---
+++ DeviceAPI/DeviceAPI/DeviceData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviceAPI
+{
+    public class DeviceData
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Brand { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+
+        public string UpdatedBy { get; set; }
+    }
+}
